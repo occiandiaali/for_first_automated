349d9c979718ac382a53758756ec3a5509da3a5d
occiandiaali <occiandiaali@gmail.com>
2025-10-26 00:08:28 +0100

    Retrieve monthly total from composable

--- src/composables/useArchives.ts
+++ src/composables/useArchives.ts
@@ -29,6 +29,25 @@ interface ArchivedItem {
   content: ArchiveContent
 }
 
+  const currentMonth = new Date().getMonth();
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const monthName = monthNames[currentMonth];
+
+  const sumTotal = ref(0)
+
 /**
  * _id:68fb71d580f5cd054c578e09
 orderNo:"d3swmtqer1"
@@ -48,13 +67,23 @@ export function useArchives() {
   const items = ref<ArchivedItem[]>([]);
   const loading = ref(false);
   const error = ref<string | null>(null);
+  //const monthTotal: number[] = []
 
 
   const fetchArchivedItems = async () => {
     loading.value = true;
     try {
       const response = await axios.get<ArchivedItem[]>('http://localhost:3000/api/admin/archive', {withCredentials:true});
+      let total = 0;
       items.value = response.data;
+          items.value.forEach(v => {
+      if (v.title === monthName) {
+      //  monthTotal.push(v.content.totalDue)
+      total += v.content.totalDue
+    }
+  })
+  sumTotal.value = total
+   // console.log("useArchives arr ",monthTotal)
     } catch (err) {
       error.value = 'Failed to fetch archived items';
       console.error(err)
@@ -62,8 +91,21 @@ export function useArchives() {
       loading.value = false;
     }
   };
+  // const fetchMonthlySales = async () => {
+
+  //   try {
+  //     const response = await axios.get<{[key: string]:number}[]>('http://localhost:3000/api/admin/monthlySales', {withCredentials:true});
+  //     monthTotal.value = response.data;
+      
+  //   } catch (error) {
+          
+  //     console.error('Failed to fetch monthTotal items', error)
+  //   }finally {
+  //     loading.value = false;
+  //   }
+  // }
 
   onMounted(fetchArchivedItems);
 
-  return { items, loading, error };
+  return { items, monthName, sumTotal, loading, error };
 }
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,33 +1,55 @@
 <template>
     <div class="summary-row">
-        <q-item>
+        <!-- <q-item style="justify-self: center; align-self: center;text-align: center;">
                     <q-item-section side top>
-                        <q-item-label style="font-size: xx-large;">3</q-item-label>
+                        <q-item-label style="font-size: x-large;">3</q-item-label>
                         <q-item-label><strong>Remaining Today</strong></q-item-label>
           <q-item-label caption>Next Delivery 10/21</q-item-label>
-          <q-icon name="star" color="yellow" />
+                            <div class="text-orange stars">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+             <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
         </q-item-section>
-        </q-item>
-                <q-item>
-                    <q-item-section side top>
-                        <q-item-label style="font-size: xx-large;">26</q-item-label>
-                        <q-item-label><strong>New</strong></q-item-label>
+        </q-item> -->
+                <q-item style="justify-self: center; align-self: center;text-align: center;">
+                    <q-item-section>
+                      <q-item-label><strong>Month Revenue</strong></q-item-label>
+                        <q-item-label style="font-size: x-large;">{{ new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: 'NGN'
+}).format(sumTotal) }}</q-item-label>
           <q-item-label caption>Last just added</q-item-label>
+          <!-- <q-icon name="star" color="yellow" />
           <q-icon name="star" color="yellow" />
+          <q-icon name="star" color="yellow" /> -->
+                  <div class="text-orange stars">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+             <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
         </q-item-section>
         </q-item>
-                <q-item>
-                    <q-item-section side top>
-                        <q-item-label style="font-size: xx-large;">6</q-item-label>
-                        <q-item-label><strong>Tomorrow</strong></q-item-label>
+                <q-item style="justify-self: center; align-self: center;text-align: center;">
+                    <q-item-section>
+                      <q-item-label><strong>Pending</strong></q-item-label>
+                        <q-item-label style="font-size: x-large;">6</q-item-label>
           <q-item-label caption>First Delivery 09:45</q-item-label>
-          <q-icon name="star" color="yellow" />
+            <div class="text-orange stars">
+            <q-icon name="star" />
+            <q-icon name="star" />
+
+          </div>
         </q-item-section>
         </q-item>
    
     </div>
     <!---->
-        <div class="summary-row">
+   
         <!-- <q-item>
                     <q-item-section side top>
                         <q-item-label style="font-size: large;">0</q-item-label>
@@ -56,10 +78,10 @@
    <div v-else-if="error">{{ error }}</div>
    <div v-else class="q-pa-sm summary-row">
   
-    <div v-for="a in items" :key="a.content.orderNo">
+    <div v-for="a in items" :key="a.content.orderNo" style="text-align: center;">
             <q-item >
-        <q-item-section side top>
-          <q-item-label overline>Total items cleaned</q-item-label>
+        <q-item-section>
+          <q-item-label overline>Top customer</q-item-label>
         
           <q-item-label style="font-size: large;">{{ a.content.garments.length }}</q-item-label>
           <q-item-label caption>{{ a.content.customer }}</q-item-label>
@@ -67,16 +89,16 @@
       </q-item>
     </div>
    </div>
-    </div>
+ 
 
 
-  <div class="q-pa-md" style="height: 260px;width: 500px;">
+  <div class="q-pa-md" style="height: 260px;width: 400px;margin-top: 5%;">
     <Line :data="data" :options="options"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted } from 'vue';
 import { useArchives } from 'src/composables/useArchives';
 import {
   Chart as ChartJS,
@@ -102,16 +124,21 @@ ChartJS.register(
 )
 
 
-const {items, loading, error} = useArchives();
+const {items, monthName, sumTotal, loading, error} = useArchives();
 
-
-//console.log(items)
-
-//console.log(typeof items)
-
-const monthTotal = ref(0)
+ console.log("MonthTotal: ", monthName)
+  console.log("SumTotal: ", sumTotal.value)
 
 const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+let thisMonthTotalSales:number[] = new Array(monthLabels.length).fill(0);
+  monthLabels.forEach((i) => {
+  const idx = monthLabels.indexOf(i);
+  if (i === monthName) {
+    thisMonthTotalSales.splice(idx, 0, sumTotal.value)
+    localStorage.setItem('thisMonthTotalSales', JSON.stringify(thisMonthTotalSales))
+  }
+})
 
 
 // Line chart config
@@ -120,8 +147,8 @@ const data = {
   datasets: [
     {
       label: 'Monthly revenue',
-      backgroundColor: '#f87979',
-      data: [400, 39, 100, 40, 39, 80, 240, 0, 0, monthTotal.value, 0, 0]
+      backgroundColor: '#00ff00',//'#f87979',
+      data: thisMonthTotalSales//[400, 39, 100, 40, 39, 80, 240, 0, 0, total.value, 0, 0]
     }
   ]
 }
@@ -131,7 +158,11 @@ const options = {
   maintainAspectRatio: false
 }
 
+onMounted(() => {
+  const data:number[] = JSON.parse(localStorage.getItem("thisMonthTotalSales") || "[]")
+  thisMonthTotalSales = data;
 
+})
 
 </script>
 
@@ -145,6 +176,9 @@ const options = {
     flex-direction: row;
     
 }
+/* .stars {
+  align-self: center;
+} */
 .summary-row {
     display: flex;
     flex-direction: row;
