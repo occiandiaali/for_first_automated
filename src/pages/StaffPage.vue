95e6a6a5227025f7e543598bb96ab473d8cfbdd7
occiandiaali <occiandiaali@gmail.com>
2025-10-27 16:57:59 +0100

    Add put & delete logic for staff

--- src/pages/StaffPage.vue
+++ src/pages/StaffPage.vue
@@ -1,14 +1,5 @@
 <template>
     <q-page padding>
-            <!-- <div class="search-row">
-          <q-input color="deep-orange" class="search-span" v-model="selectStaff" label="Search">
-        <template v-slot:prepend>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-       <q-btn square color="deep-orange" icon="add" id="new-staff" size="xs" @click="addToRow"/>
-      </div> -->
-      
         <q-btn square color="deep-orange" icon="add" id="new-staff" @click="addToRow"/>
       
       <br/>
@@ -36,7 +27,7 @@
             color="primary"
             dense
             flat
-            @click="editRow(props.row)"
+            @click="editRow(props.row, props.row._id)"
           />
           <q-btn
           size="sm"
@@ -44,7 +35,7 @@
             color="negative"
             dense
             flat
-            @click="deleteRow(props.row._id)"
+            @click="deleteRow(props.row, props.row._id)"
           />
         </q-td>
       </template>
@@ -66,14 +57,14 @@
         <br/>
                 <q-card-section class="q-pt-none">
           <q-item-label caption>user role</q-item-label>
-                                <q-radio v-model="role" val="admin" label="Admin" />
+          <q-radio v-model="role" val="admin" label="Admin" />
       <q-radio v-model="role" val="user" label="User" />
-      <q-item-label v-if="role" caption>Default password: {{ role }}</q-item-label>
+      <q-item-label v-if="addAction" caption>Default password: {{ role }}</q-item-label>
         </q-card-section>
 
         <q-card-actions align="right" class="text-primary">
           <q-btn flat label="Cancel" v-close-popup />
-          <q-btn flat label="Add user" v-close-popup @click="addNewUser(name, email, role)" />
+          <q-btn flat :label="addAction ? 'Add user' : 'Save'" v-close-popup @click="addAction ? addNewUser(name, email, role) : updateUser(name, email, role)" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -86,69 +77,14 @@ import {  ref } from 'vue';
 import axios from 'axios';
 import { useUsers } from 'src/composables/useUsers';
 
-const {users, loading, error} = useUsers()
+const {users, loading, error} = useUsers(); // Staff data
 
+// To please TS for the table
 type RowType = [number, string, string, string]
 type AlignType = "left" | "center" | "right";
 
-//const selectStaff = ref()
-const name = ref()
-const email = ref()
-const prompt = ref(false)
-const role = ref()
+ const staff = ref(users); // The rows data 
 
-const addAction = ref(true)
-const  userInFocus = ref<string|undefined>(undefined)
-const  emailInFocus = ref<string|undefined>(undefined)
-
-function addNewUser(uname:string,umail:string,urole:string ) {
-  const defaultPassword = urole;
-  //alert(`New User: ${uname}, ${umail}, ${urole} - password:${defaultPassword}`)
-
-  try {
-    const userData = {
-      username:uname,
-      email:umail,
-      password:defaultPassword,
-      role:urole,
-  
-    }
-    axios.post('http://localhost:3000/api/auth/register', userData)
-    .then(response => {
-      console.log('User saved: ', response.data);
-            name.value = null;
-  email.value = null;
-  role.value = null;
-    })
-    .catch(error => console.error('Error: ', error))
-    // const response = await fetch('http://localhost:6666/api/auth/register', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify(userData)
-    // });
-    // const data = await response.json();
-    
-
-  } catch (error) {
-    console.error('Err saving user: ', error)
-  }
-}
-
-// const columns = [
-//         { name: 'email', label: 'Email', field: 'email', align: 'left' as AlignType },
-//         { name: 'name', label: 'Name', field: 'name', align: 'left' as AlignType},
-//         { name: 'role', label: 'Role', field: 'role', align: 'left' as AlignType},
-//         { name: 'actions', label: '', field: 'actions', align: 'center' as AlignType}
-//       ]
-
- const staff = ref(users);      
-// let rows = [
-//         { id: 1, email: 'admin@example.com', name: 'Admin', role: 'Admin' },
-//         { id: 2, email: 'bob@example.com', name: 'Bob', role: 'Editor' },
-//         { id: 3, email: 'carol@example.com', name: 'Carol', role: 'Viewer' }
-//       ]
 const columns = [
         { name: 'email', label: 'Email', field: 'email', align: 'left' as AlignType },
         { name: 'username', label: 'Name', field: 'username', align: 'left' as AlignType},
@@ -156,68 +92,115 @@ const columns = [
         { name: 'actions', label: '', field: 'actions', align: 'center' as AlignType}
       ]
 
-//const staff = ref([]);      
+const name = ref()
+const email = ref()
+const prompt = ref(false)
+const role = ref()
 
+const addAction = ref(true)
+const docID = ref()
+const  userInFocus = ref<string|undefined>(undefined)
+const  emailInFocus = ref<string|undefined>(undefined)
+const roleInFocus = ref<string|undefined>(undefined)
+
+function addNewUser(uname:string,umail:string,urole:string ) {
+  const defaultPassword = urole;
+  try {
+    const userData = {
+      username:uname,
+      email:umail,
+      password:defaultPassword,
+      role:urole,
+    }
+    axios.post('http://localhost:3000/api/auth/register', userData, {withCredentials:true})
+    .then(response => {
+      console.log('User saved: ', response.data);
+      name.value = null;
+      email.value = null;
+      role.value = null;
+      location.reload();
+    })
+    .catch(error => console.error('Error: ', error))
+  } catch (error) {
+    console.error('Err saving user: ', error)
+  }
+}
+
+async function updateUser(uname:string,umail:string,urole:string ) {
+  const updateUser = {
+      username:uname,
+      email:umail,
+      role:urole,
+  };
+  try {
+    const response = await axios.put(`http://localhost:3000/api/admin/staff/${docID.value}`, updateUser, {withCredentials:true})
+   
+      console.log('User updated: ', response.data);
+      name.value = null;
+      email.value = null;
+      role.value = null;
+      location.reload();
+ 
+  } catch (error) {
+    console.error('Err updating user: ', error)
+  }
+}
 
 function addToRow() {
     prompt.value = true;
   addAction.value = true;
+  name.value = null;
+  email.value = null;
+  role.value = null;
 }
 
-function editRow(row: RowType[]) {
+function editRow(row: RowType, id:string) {
   prompt.value = true;
   addAction.value = false;
+  docID.value = id;
 
-  //console.log('Edit:', row);
   for (const [key, value] of Object.entries(row)) {
    
-    if (key === "username") {
+    if (addAction.value === false && key === "username") {
       userInFocus.value = value as unknown as string;
-      
+      name.value = userInFocus.value
       
     }
-        if (key === "email") {
+    if (addAction.value === false && key === "email") {
       emailInFocus.value = value as unknown as string;
+      email.value = emailInFocus.value
       
+    }
+        if (addAction.value === false && key === "role") {
+      roleInFocus.value = value as unknown as string;
+      role.value = roleInFocus.value
       
     }
 
   }
     }
-// function deleteRow(id:number) {
-//       rows = rows.filter(row => row.id !== id);
-//     }
-function deleteRow(id:string) {
-      staff.value = staff.value.filter(row => row._id !== id);
+
+ function deleteRow(row: RowType, id:string) {
+    for (const [key,value] of Object.entries(row)) {
+        if (key === "username") {
+          if (confirm(`Delete access for ${value}?`)) {
+            axios.delete(`http://localhost:3000/api/admin/staff/${id}`)
+            .then((response) => {
+              location.reload();
+              console.log(response.data);
+            }).catch(e => console.error(e))
+          console.log(`${value} deleted!`)
+          }
+        }
+      }
     }
-
- 
-
-// onMounted(() => {
-//   console.log("fetchStaff ", users)
-//   console.error("ifError ", error)
-
-// })
 </script>
 
 <style lang="css" scoped>
 #new-staff {
     margin: 6px;
 }
-/*
-.search-row {
-    display: flex;
-    flex-direction: row;
-    padding: 4px;
-   
-    position: fixed;
-    width: 100%;
-    z-index: 50;
-}
-.search-span {
-    width: 96%;
-}
-    */
+
 #table-div {
   margin-top: 5%;
 }
